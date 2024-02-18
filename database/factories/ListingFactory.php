2bb2c0ede5d55cb854d927be4baf40e00919d8e3
g0gab1s <83614025+GabrielaGodek@users.noreply.github.com>
2024-02-18 23:37:24 +0100

    setup mysql and basic layout with tailwind

---
+++ database/factories/ListingFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Listing>
+ */
+class ListingFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "title" => $this->faker->sentence(),
+            "description" => "Seasonal coffee with various flavor notes like caramel, fruit, and chocolate",
+            "ingredients" => "Espresso, Espresso, Espresso",
+            "proportions" => "N/A",
+            "image" => "https://images.unsplash.com/photo-1611162458324-aae1eb4129a4?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTg1fHxibGFjayUyMGNvZmZlZXxlbnwwfHwwfHx8MA%3D%3D",
+        ];
+    }
+}
