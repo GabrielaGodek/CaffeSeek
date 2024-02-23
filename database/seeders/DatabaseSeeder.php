cc829fa5efbc4b2850d4026b93e62c5d98ccbedc
g0gab1s <83614025+GabrielaGodek@users.noreply.github.com>
2024-02-23 10:30:16 +0100

    deploy to vercel

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,17 +3,12 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Database\Seeders\ListingTable;
 
 use Illuminate\Database\Seeder;
 use Database\Seeders\ListingTableSeeder;
-use TimoKoerber\LaravelJsonSeeder\JsonDatabaseSeeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
         $this->call(ListingTableSeeder::class);
